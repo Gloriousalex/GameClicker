95fd4dff0c7a3f8235f1f80580237decead6d55d
Gloriousalex <alexglorious@gmail.com>
2022-07-27 10:22:54 +0300

    Add carts of russians an logic

--- cards.js
+++ cards.js
@@ -2,7 +2,96 @@ const russians = [
       {
       id: 0,
       level: 1,
-      imageurl: '',
-      clickcounts: 10 
+      clickcounts: 10, 
+      cards: [
+        {
+        info: 'На картинке всё сказано',
+        name: 'patriot_1.jpg',
+      },
+      {
+        info: 'Типичное русское быдло',
+        name: 'patriot_2.jpg',
+      },
+      {
+        info: 'Типичное русское быдло',
+        name: 'patriot_3.jpg',
+      }
+    ],      
+  },
+  {
+    id: 1,
+    level: 2,
+    cards: [
+      {
+      info: 'Пропагандонка Чичерина',
+      name: 'star_1.jpg',
+    },
+    {
+      info: 'Любимец ватника - Лепс',
+      name: 'star_2.jpg',
+    },
+    {
+      info: 'Золотой рот россии',
+      name: 'star_3.jpg',
+    }
+  ],
+    clickcounts: 15, 
+},
+{
+  id: 2,
+  level: 3,
+  cards: [
+    {
+    info: 'Пропагандонка Скабеева',
+    name: 'propagandon_1.jpg',
+  },
+  {
+    info: 'Жена Соловьева и просто неадекватка Симоньян',
+    name: 'propagandon_2.jpg',
+  },
+  {
+    info: 'Рупор мысли путлера Соловьев',
+    name: 'propagandon_3.jpg',
   }
+],
+  clickcounts: 20, 
+},
+{
+  id: 3,
+  level: 4,
+  cards: [
+    {
+    info: 'Виновник всего - путлер',
+    name: 'presic_1.jpg',
+  },
+  {
+    info: 'Прихлебатель пуйла',
+    name: 'presic_2.jpg',
+  },
+  {
+    info: 'Троянский конь в Европе',
+    name: 'presic_3.jpg',
+  }
+],
+  clickcounts: 25, 
+},
+{
+  id: 4,
+  level: 5,
+  cards: [
+    {
+    info: 'Босс игры - Чмоня!',
+    name: 'boss_1.jpg',
+  },
+  {
+    info: 'Финальный босс - кум путлера',
+    name: 'boss_2.jpg',
+  },
+  {
+    info: 'Конец игры - Медведчук в окружении',
+    name: 'boss_3.jpg',
+  }
+],
+  clickcounts: 30, 
+}
 ];
--- main.js
+++ main.js
@@ -1,6 +1,7 @@
+// Functions set
 
-
-function retrieveFormData (event) {
+function retrieveFormData (event) { //Retrieve Data from Form and write it to LocalStorage
+  event.preventDefault()
   const fname = form.querySelector("[name='fname']"),
         lname = form.querySelector("[name='lname']"),
         uname = form.querySelector("[name='uname']"),
@@ -14,40 +15,153 @@ function retrieveFormData (event) {
   }
   
   localStorage.setItem('UserData', JSON.stringify(registrationData));
-  addRightSideBar(registrationData);
+  formStart.classList.add('hiddenBar')  
+  playGame();
 }
 
-function addRightSideBar (userData) {
+function addRightSideBar (userData,gameData) { //Adding Right Side Bar, get items from LocalStorage
   const sideBar = document.querySelector('.sideBar');
-  sideBar.classList.replace('hiddenBar', 'activeBar')
-  sideBarValue =
-  `
-  <fieldset class="userInfoField">
-  <legend>User Detail</legend><h2>User Detail</h2>
+  sideBar.classList.replace('hiddenBar', 'activeBar');
+  const sideBarValue =
+  `<div class="userInfoField">
+  <fieldset >
+  <legend>User Detail</legend>
   <div>
   <h3>User Name</h3>
   <p class="userInfo">${userData.fname} ${userData.lname}</p>
-  <p class="userName">${userData.uname}</p>
+  <h3>Nickname</h3>
+  <p class="userInfo">${userData.uname}</p>
+  <h3>Mail</h3>
   <p class="userInfo">${userData.mail}</p>
+  <h3>Level</h3>
+  <p class="userInfo">${gameData.level}</p>
   </fieldset>
+  <button id="logout">Log Out</button>
+  </div>
   `
-  sideBar.insertAdjacentHTML('afterbegin',sideBarValue);
+  sideBar.insertAdjacentHTML('afterbegin',sideBarValue); 
 }
 
-function registrationStart () {
+function registrationStart () { //Registration Form
   const form = document.querySelector('#form');
-  formStart = document.querySelector('.form-class')
+  console.log('Registration Form', form);
+  const rightBar = document.querySelector('.sideBar');
+  rightBar.classList.replace('activeBar', 'hiddenBar');
+
   formStart.classList.remove('hiddenBar')
   form.addEventListener('submit', retrieveFormData);
 }
 
-const localitem = JSON.parse(localStorage.getItem('UserData'));
+function randomize (count) { //randoms length of array with image
+  let num = Math.random()*count;
+  return Math.round(num);
+}
 
-if (localitem === null) {
-  registrationStart();
-} else {
-  addRightSideBar(localitem)
+function findCard (images) { //finds random image from current lvl
+  const newImage = {...images}
+  const imageCounter = images.cards.length -1;
+  const randomImg = randomize(imageCounter);
+  newImage.cards = images.cards[randomImg];
+  return newImage;
 }
 
 
+function russiansImage (gameData, userData) { //Main Game function
+  if (!gameData) {
+    localStorage.setItem('gameData', JSON.stringify(findCard(russians[0])))
+    playGame();
+  }
+  else { 
+    const gameDiv = document.querySelector('.container')
+    const rusTerValue = `<div class ="imageField">
+    <h1>${gameData.cards.info}</h1>
+    <img src ="./IMG/${gameData.cards.name}" id="imageRus">
+    <div id="counter">
+    <div class = "Counterclass">
+    <p>Clicks left: <strong id="clickcount">${gameData.clickcounts}</strong></p>
+    </div>
+    </div>
+    </div>
+    `
+    gameDiv.insertAdjacentHTML('afterbegin', rusTerValue); //insert main window
+    addRightSideBar(userData,gameData);    
+    let clickcounts = gameData.clickcounts;
+
+    const logOutBTN = document.querySelector('#logout');
+
+    const imageClicker = document.querySelector('#imageRus')
+    const newTextCounter = document.querySelector('#clickcount')
+
+    imageClicker.addEventListener('click', () => { //main clicker - listener
+      clickcounts--;
+      newTextCounter.textContent = `${clickcounts}`
+      if (clickcounts === 0) {
+        const gameLevel = gameData.id+1;  //Прописать победителя
+        if (gameLevel > russians.length-1) {
+          victory(gameData,userData);
+        } else {
+        clearData();
+        localStorage.setItem('gameData', JSON.stringify(findCard(russians[gameLevel])))
+        playGame()
+        }
+      }
+    });
+  
+    logOutBTN.addEventListener('click', () => { //logout button with erase data
+      clearData();  
+      localStorage.removeItem('UserData');
+      localStorage.removeItem('gameData');
+      playGame();
+      
+    });
+  }
+}
+
+function victory (gameData, userData) {
+    clearData();
+    const gameDiv = document.querySelector('.container');
+    const victoryValue = `<div class ="imageField">
+    <h1>Congratulations, ${userData.uname}!</h1>
+    <h3>You defeated all bosses and bring peace all around the world!</h3>
+    <h3>Click "again" to defeat them one more time</h3>
+    <p>some images may change :)</p>
+    <button class="btn">Again</button>  
+    </div>`
+    gameDiv.insertAdjacentHTML('afterbegin', victoryValue);
+    const againBtn = gameDiv.querySelector('.btn');
+    againBtn.addEventListener('click', () => { //Не перезапускает, так как уже удалено геймдата
+      const imageData = document.querySelector('.imageField');
+      imageData.remove();
+      localStorage.removeItem('UserData');
+      localStorage.removeItem('gameData');
+      playGame();
+    })
+}
+
+function clearData () {  //clears data from  storage and some windows
+  const sideUserData = document.querySelector(`.userInfoField`)
+  const imageData = document.querySelector('.imageField');
+  
+  sideUserData.remove();
+  imageData.remove();
+}
+
+function playGame() {  //Main Code for playing game
+  const userData = JSON.parse(localStorage.getItem('UserData'));
+  const gameData = JSON.parse(localStorage.getItem('gameData'));
+
+  if (!userData) {
+    registrationStart();
+  } else {
+    // addRightSideBar(userData);
+    russiansImage(gameData, userData)
+  }
+
+}
+
+// Playing Game
+
+const formStart = document.querySelector('.form-class')
+const container = document.querySelector('.container')
+playGame();
 
